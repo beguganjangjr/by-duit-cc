c50bdcd062a1b84b62570f6cb7448d42dd1c22c7
beguganjangjr <64639270+beguganjangjr@users.noreply.github.com>
2021-08-30 18:07:46 +0700

    Update dcc.py

--- dcc.py
+++ dcc.py
@@ -25,8 +25,7 @@ def ByPas(url, referer=True):
 	print(cookie)
 	if cookie:
 		headers.update({'Cookie': cookie})
-	cookies = cookie
-	html = r.get(url, cookies=cookies).text
+	html = get_.content
 	if not referer:
 		header.udpate({'Cookie': cookie})
 	headers.update({'Origin': rurl[:-1]})
@@ -38,6 +37,58 @@ def ByPas(url, referer=True):
 def scrape_sources(html, result_blacklist=None, scheme='http', patterns=None, generic_patterns=True):
     if patterns is None:
         patterns = []
+
+    def __parse_to_list(_html, regex):
+        _blacklist = ['.jpg', '.jpeg', '.gif', '.png', '.js', '.css', '.htm', '.html', '.php', '.srt', '.sub', '.xml', '.swf', '.vtt', '.mpd']
+        _blacklist = set(_blacklist + result_blacklist)
+        streams = []
+        labels = []
+        for r in re.finditer(regex, _html, re.DOTALL):
+            match = r.groupdict()
+            stream_url = match['url'].replace('&amp;', '&')
+            file_name = urllib_parse.urlparse(stream_url[:-1]).path.split('/')[-1] if stream_url.endswith("/") else urllib_parse.urlparse(stream_url).path.split('/')[-1]
+            label = match.get('label', file_name)
+            if label is None:
+                label = file_name
+            blocked = not file_name or any(item in file_name.lower() for item in _blacklist) or any(item in label for item in _blacklist)
+            if stream_url.startswith('//'):
+                stream_url = scheme + ':' + stream_url
+            if '://' not in stream_url or blocked or (stream_url in streams) or any(stream_url == t[1] for t in source_list):
+                continue
+            labels.append(label)
+            streams.append(stream_url)
+
+        matches = zip(labels, streams) if six.PY2 else list(zip(labels, streams))
+        if matches:
+            common.logger.log_debug('Scrape sources |%s| found |%s|' % (regex, matches))
+        return matches
+
+    if result_blacklist is None:
+        result_blacklist = []
+    elif isinstance(result_blacklist, str):
+        result_blacklist = [result_blacklist]
+
+    html = html.replace(r"\/", "/")
+    html += get_packed_data(html)
+
+    source_list = []
+    if generic_patterns or not patterns:
+        source_list += __parse_to_list(html, r'''["']?label\s*["']?\s*[:=]\s*["']?(?P<label>[^"',]+)["']?(?:[^}\]]+)["']?\s*file\s*["']?\s*[:=,]?\s*["'](?P<url>[^"']+)''')
+        source_list += __parse_to_list(html, r'''["']?\s*(?:file|src)\s*["']?\s*[:=,]?\s*["'](?P<url>[^"']+)(?:[^}>\]]+)["']?\s*label\s*["']?\s*[:=]\s*["']?(?P<label>[^"',]+)''')
+        source_list += __parse_to_list(html, r'''video[^><]+src\s*[=:]\s*['"](?P<url>[^'"]+)''')
+        source_list += __parse_to_list(html, r'''source\s+src\s*=\s*['"](?P<url>[^'"]+)['"](?:.*?res\s*=\s*['"](?P<label>[^'"]+))?''')
+        source_list += __parse_to_list(html, r'''["'](?:file|url)["']\s*[:=]\s*["'](?P<url>[^"']+)''')
+        source_list += __parse_to_list(html, r'''param\s+name\s*=\s*"src"\s*value\s*=\s*"(?P<url>[^"]+)''')
+    for regex in patterns:
+        source_list += __parse_to_list(html, regex)
+
+    source_list = list(set(source_list))
+
+    common.logger.log(source_list)
+    source_list = sort_sources_list(source_list)
+
+    return source_list
+
 if __name__ == "__main__":
 	if len(argv) < 2:
 		exit(f'Jalankan : python {argv[0]} <url>')
